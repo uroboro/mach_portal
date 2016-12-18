fa39754773189bec147b2b6574d3b6c57193dd06
uroboro <uroboro.845@gmail.com>
2016-12-18 19:54:40 -0300

    Added more (22) devices from Mila432.
    Some offsets are the same so next update can remove unnecessary functions.

--- mach_portal/offsets.c
+++ mach_portal/offsets.c
@@ -87,6 +87,10 @@ void init_iPhone6_1_S5L8960X() {
 	allproc_offset = 0x5a4128;
 	kernproc_offset = 0x5aa0e0;
 }
+void init_iPhone6_2_S5L8960X() {
+	allproc_offset = 0x5a4128;
+	kernproc_offset = 0x5aa0e0;
+}
 void init_iPhone7_1_T7000() {
 	allproc_offset = 0x5b4168;
 	kernproc_offset = 0x5ba0e0;
@@ -127,6 +131,10 @@ void init_iPhone9_1_T8010() {
 	allproc_offset = 0x5ec178;
 	kernproc_offset = 0x5f20e0;
 }
+void init_iPhone9_2_T8010() {
+	allproc_offset = 0x5ec178;
+	kernproc_offset = 0x5f20e0;
+}
 void init_iPhone9_3_T8010() {
 	allproc_offset = 0x5ec178;
 	kernproc_offset = 0x5f20e0;
@@ -145,11 +153,63 @@ void init_iPod7_1_T7000() {
 
 #pragma mark - iPad
 
+void init_iPad4_1_S5L8960X() {
+	allproc_offset = 0x5a4128;
+	kernproc_offset = 0x5aa0e0;
+}
+void init_iPad4_2_S5L8960X() {
+	allproc_offset = 0x5a4128;
+	kernproc_offset = 0x5aa0e0;
+}
 void init_iPad4_3_S5L8960X() {
 	allproc_offset = 0x5a4128;
 	kernproc_offset = 0x5aa0e0;
 }
 void init_iPad4_4_S5L8960X() {
+	allproc_offset = 0x5a4128;
+	kernproc_offset = 0x5aa0e0;
+}
+void init_iPad4_5_S5L8960X() {
+	allproc_offset = 0x5a4128;
+	kernproc_offset = 0x5aa0e0;
+}
+void init_iPad4_6_S5L8960X() {
+	allproc_offset = 0x5a4128;
+	kernproc_offset = 0x5aa0e0;
+}
+void init_iPad4_7_T7000() {
+	allproc_offset = 0x5b4168;
+	kernproc_offset = 0x5ba0e0;
+}
+void init_iPad4_7_T7001() {
+	allproc_offset = 0x5b4228;
+	kernproc_offset = 0x5ba0e0;
+}
+void init_iPad4_7_S5L8960X() {
+	allproc_offset = 0x5a4128;
+	kernproc_offset = 0x5aa0e0;
+}
+void init_iPad4_8_T7000() {
+	allproc_offset = 0x5b4168;
+	kernproc_offset = 0x5ba0e0;
+}
+void init_iPad4_8_T7001() {
+	allproc_offset = 0x5b4228;
+	kernproc_offset = 0x5ba0e0;
+}
+void init_iPad4_8_S5L8960X() {
+	allproc_offset = 0x5a4128;
+	kernproc_offset = 0x5aa0e0;
+}
+void init_iPad4_9_T7000() {
+	allproc_offset = 0x5b4168;
+	kernproc_offset = 0x5ba0e0;
+}
+void init_iPad4_9_T7001() {
+	allproc_offset = 0x5b4228;
+	kernproc_offset = 0x5ba0e0;
+}
+void init_iPad4_9_S5L8960X() {
 	allproc_offset = 0x5a4128;
 	kernproc_offset = 0x5aa0e0;
 }
@@ -162,6 +222,30 @@ void init_iPad5_1_T7001() {
 	kernproc_offset = 0x5ba0e0;
 }
 void init_iPad5_1_S5L8960X() {
+	allproc_offset = 0x5a4128;
+	kernproc_offset = 0x5aa0e0;
+}
+void init_iPad5_2_T7000() {
+	allproc_offset = 0x5b4168;
+	kernproc_offset = 0x5ba0e0;
+}
+void init_iPad5_2_T7001() {
+	allproc_offset = 0x5b4228;
+	kernproc_offset = 0x5ba0e0;
+}
+void init_iPad5_2_S5L8960X() {
+	allproc_offset = 0x5a4128;
+	kernproc_offset = 0x5aa0e0;
+}
+void init_iPad5_3_T7000() {
+	allproc_offset = 0x5b4168;
+	kernproc_offset = 0x5ba0e0;
+}
+void init_iPad5_3_T7001() {
+	allproc_offset = 0x5b4228;
+	kernproc_offset = 0x5ba0e0;
+}
+void init_iPad5_3_S5L8960X() {
 	allproc_offset = 0x5a4128;
 	kernproc_offset = 0x5aa0e0;
 }
@@ -185,10 +269,16 @@ void init_iPad6_4_S8000() {
 	allproc_offset = 0x5a4148;
 	kernproc_offset = 0x5aa0e0;
 }
+void init_iPad6_7_S8000() {
+	allproc_offset = 0x5a4148;
+	kernproc_offset = 0x5aa0e0;
+}
 void init_iPad6_8_S8000() {
 	allproc_offset = 0x5a4148;
 	kernproc_offset = 0x5aa0e0;
 }
+
+#pragma mark - to add
 
 //here end
 void unknown_build() {
@@ -219,95 +309,135 @@ void init_offsets() {
 	
 	init_arm64();
 	
+	char * model = NULL;
+	
 #pragma mark - iPhone
 	
-	if (strstr(u.machine, "iPhone6,1")) {
+#pragma mark iPhone6,1
+	model = "iPhone6,1";
+	if (strstr(u.machine, model)) {
 		if (strstr(u.version, "root:xnu-3789.22.3~1/RELEASE_ARM64_S5L8960X")) {
-			printf("device iPhone6,1 matched\n");
+			printf("device %s matched\n", model);
 			init_iPhone6_1_S5L8960X();
 			return;
 		}
 	}
 	
-	if (strstr(u.machine, "iPhone7,1")) {
+#pragma mark iPhone6,2
+	model = "iPhone6,2";
+	if (strstr(u.machine, model)) {
+		if (strstr(u.version, "root:xnu-3789.22.3~1/RELEASE_ARM64_S5L8960X")) {
+			printf("device %s matched\n", model);
+			init_iPhone6_2_S5L8960X();
+			return;
+		}
+	}
+	
+#pragma mark iPhone7,1
+	model = "iPhone7,1";
+	if (strstr(u.machine, model)) {
 		if (strstr(u.version, "root:xnu-3789.22.3~1/RELEASE_ARM64_T7000")) {
-			printf("device iPhone7,1 matched\n");
+			printf("device %s matched\n", model);
 			init_iPhone7_1_T7000();
 			return;
 		}
 		if (strstr(u.version, "root:xnu-3789.22.3~1/RELEASE_ARM64_S8000")) {
-			printf("device iPhone7,1 matched\n");
+			printf("device %s matched\n", model);
 			init_iPhone7_1_S8000();
 			return;
 		}
 	}
 	
-	if (strstr(u.machine, "iPhone7,2")) {
+#pragma mark iPhone7,2
+	model = "iPhone7,2";
+	if (strstr(u.machine, model)) {
 		if (strstr(u.version, "root:xnu-3789.22.3~1/RELEASE_ARM64_T7000")) {
-			printf("device iPhone7,2 matched\n");
+			printf("device %s matched\n", model);
 			init_iPhone7_2_T7000();
 			return;
 		}
 		if (strstr(u.version, "root:xnu-3789.22.3~1/RELEASE_ARM64_S8000")) {
-			printf("device iPhone7,2 matched\n");
+			printf("device %s matched\n", model);
 			init_iPhone7_2_S8000();
 			return;
 		}
 	}
 	
-	if (strstr(u.machine, "iPhone8,1")) {
+#pragma mark iPhone8,1
+	model = "iPhone8,1";
+	if (strstr(u.machine, model)) {
 		if (strstr(u.version, "root:xnu-3789.22.3~1/RELEASE_ARM64_T7000")) {
-			printf("device iPhone8,1 matched\n");
+			printf("device %s matched\n", model);
 			init_iPhone8_1_T7000();
 			return;
 		}
 		if (strstr(u.version, "root:xnu-3789.22.3~1/RELEASE_ARM64_S8000")) {
-			printf("device iPhone8,1 matched\n");
+			printf("device %s matched\n", model);
 			init_iPhone8_1_S8000();
 			return;
 		}
 	}
 	
-	if (strstr(u.machine, "iPhone8,2")) {
+#pragma mark iPhone8,2
+	model = "iPhone8,2";
+	if (strstr(u.machine, model)) {
 		if (strstr(u.version, "root:xnu-3789.22.3~1/RELEASE_ARM64_T7000")) {
-			printf("device iPhone8,2 matched\n");
+			printf("device %s matched\n", model);
 			init_iPhone8_2_T7000();
 			return;
 		}
 		if (strstr(u.version, "root:xnu-3789.22.3~1/RELEASE_ARM64_S8000")) {
-			printf("device iPhone8,2 matched\n");
+			printf("device %s matched\n", model);
 			init_iPhone8_2_S8000();
 			return;
 		}
 	}
 	
-	if (strstr(u.machine, "iPhone8,4")) {
+#pragma mark iPhone8,4
+	model = "iPhone8,4";
+	if (strstr(u.machine, model)) {
 		if (strstr(u.version, "root:xnu-3789.22.3~1/RELEASE_ARM64_S8000")) {
-			printf("device iPhone8,4 matched\n");
+			printf("device %s matched\n", model);
 			init_iPhone8_4_S8000();
 			return;
 		}
 	}
 	
-	if (strstr(u.machine, "iPhone9,1")) {
+#pragma mark iPhone9,1
+	model = "iPhone9,1";
+	if (strstr(u.machine, model)) {
 		if (strstr(u.version, "root:xnu-3789.22.3~1/RELEASE_ARM64_T8010")) {
-			printf("device iPhone9,1 matched\n");
+			printf("device %s matched\n", model);
 			init_iPhone9_1_T8010();
 			return;
 		}
 	}
 	
-	if (strstr(u.machine, "iPhone9,3")) {
+#pragma mark iPhone9,2
+	model = "iPhone9,2";
+	if (strstr(u.machine, model)) {
 		if (strstr(u.version, "root:xnu-3789.22.3~1/RELEASE_ARM64_T8010")) {
-			printf("device iPhone9,3 matched\n");
+			printf("device %s matched\n", model);
+			init_iPhone9_2_T8010();
+			return;
+		}
+	}
+	
+#pragma mark iPhone9,3
+	model = "iPhone9,3";
+	if (strstr(u.machine, model)) {
+		if (strstr(u.version, "root:xnu-3789.22.3~1/RELEASE_ARM64_T8010")) {
+			printf("device %s matched\n", model);
 			init_iPhone9_3_T8010();
 			return;
 		}
 	}
 	
-	if (strstr(u.machine, "iPhone9,4")) {
+#pragma mark iPhone9,4
+	model = "iPhone9,4";
+	if (strstr(u.machine, model)) {
 		if (strstr(u.version, "root:xnu-3789.22.3~1/RELEASE_ARM64_T8010")) {
-			printf("device iPhone9,4 matched\n");
+			printf("device %s matched\n", model);
 			init_iPhone9_4_T8010();
 			return;
 		}
@@ -315,9 +445,11 @@ void init_offsets() {
 	
 #pragma mark - iPod
 	
-	if (strstr(u.machine, "iPod7,1")) {
+#pragma mark iPod7,1
+	model = "iPod7,1";
+	if (strstr(u.machine, model)) {
 		if (strstr(u.version, "root:xnu-3789.22.3~1/RELEASE_ARM64_T7000")) {
-			printf("device iPod7,1 matched\n");
+			printf("device %s matched\n", model);
 			init_iPod7_1_T7000();
 			return;
 		}
@@ -325,77 +457,241 @@ void init_offsets() {
 	
 #pragma mark - iPad
 	
-	if (strstr(u.machine, "iPad4,3")) {
+#pragma mark iPad4,1
+	model = "iPad4,1";
+	if (strstr(u.machine, model)) {
 		if (strstr(u.version, "root:xnu-3789.22.3~1/RELEASE_ARM64_S5L8960X")) {
-			printf("device iPad4,3 matched\n");
+			printf("device %s matched\n", model);
+			init_iPad4_1_S5L8960X();
+			return;
+		}
+	}
+	
+#pragma mark iPad4,2
+	model = "iPad4,2";
+	if (strstr(u.machine, model)) {
+		if (strstr(u.version, "root:xnu-3789.22.3~1/RELEASE_ARM64_S5L8960X")) {
+			printf("device %s matched\n", model);
+			init_iPad4_2_S5L8960X();
+			return;
+		}
+	}
+	
+#pragma mark iPad4,3
+	model = "iPad4,3";
+	if (strstr(u.machine, model)) {
+		if (strstr(u.version, "root:xnu-3789.22.3~1/RELEASE_ARM64_S5L8960X")) {
+			printf("device %s matched\n", model);
 			init_iPad4_3_S5L8960X();
 			return;
 		}
 	}
 	
-	if (strstr(u.machine, "iPad4,4")) {
+#pragma mark iPad4,4
+	model = "iPad4,4";
+	if (strstr(u.machine, model)) {
 		if (strstr(u.version, "root:xnu-3789.22.3~1/RELEASE_ARM64_S5L8960X")) {
-			printf("device iPad4,4 matched\n");
+			printf("device %s matched\n", model);
 			init_iPad4_4_S5L8960X();
 			return;
 		}
 	}
 	
-	if (strstr(u.machine, "iPad5,1")) {
+#pragma mark iPad4,5
+	model = "iPad4,5";
+	if (strstr(u.machine, model)) {
+		if (strstr(u.version, "root:xnu-3789.22.3~1/RELEASE_ARM64_S5L8960X")) {
+			printf("device %s matched\n", model);
+			init_iPad4_5_S5L8960X();
+			return;
+		}
+	}
+	
+#pragma mark iPad4,6
+	model = "iPad4,6";
+	if (strstr(u.machine, model)) {
+		if (strstr(u.version, "root:xnu-3789.22.3~1/RELEASE_ARM64_S5L8960X")) {
+			printf("device %s matched\n", model);
+			init_iPad4_6_S5L8960X();
+			return;
+		}
+	}
+	
+#pragma mark iPad4,7
+	model = "iPad4,7";
+	if (strstr(u.machine, model)) {
 		if (strstr(u.version, "root:xnu-3789.22.3~1/RELEASE_ARM64_T7000")) {
-			printf("device iPad5,1 matched\n");
+			printf("device %s matched\n", model);
+			init_iPad4_7_T7000();
+			return;
+		}
+		if (strstr(u.version, "root:xnu-3789.22.3~1/RELEASE_ARM64_T7001")) {
+			printf("device %s matched\n", model);
+			init_iPad4_7_T7001();
+			return;
+		}
+		if (strstr(u.version, "root:xnu-3789.22.3~1/RELEASE_ARM64_S5L8960X")) {
+			printf("device %s matched\n", model);
+			init_iPad4_7_S5L8960X();
+			return;
+		}
+	}
+	
+#pragma mark iPad4,8
+	model = "iPad4,8";
+	if (strstr(u.machine, model)) {
+		if (strstr(u.version, "root:xnu-3789.22.3~1/RELEASE_ARM64_T7000")) {
+			printf("device %s matched\n", model);
+			init_iPad4_8_T7000();
+			return;
+		}
+		if (strstr(u.version, "root:xnu-3789.22.3~1/RELEASE_ARM64_T7001")) {
+			printf("device %s matched\n", model);
+			init_iPad4_8_T7001();
+			return;
+		}
+		if (strstr(u.version, "root:xnu-3789.22.3~1/RELEASE_ARM64_S5L8960X")) {
+			printf("device %s matched\n", model);
+			init_iPad4_8_S5L8960X();
+			return;
+		}
+	}
+	
+#pragma mark iPad4,9
+	model = "iPad4,9";
+	if (strstr(u.machine, model)) {
+		if (strstr(u.version, "root:xnu-3789.22.3~1/RELEASE_ARM64_T7000")) {
+			printf("device %s matched\n", model);
+			init_iPad4_9_T7000();
+			return;
+		}
+		if (strstr(u.version, "root:xnu-3789.22.3~1/RELEASE_ARM64_T7001")) {
+			printf("device %s matched\n", model);
+			init_iPad4_9_T7001();
+			return;
+		}
+		if (strstr(u.version, "root:xnu-3789.22.3~1/RELEASE_ARM64_S5L8960X")) {
+			printf("device %s matched\n", model);
+			init_iPad4_9_S5L8960X();
+			return;
+		}
+	}
+	
+#pragma mark iPad5,1
+	model = "iPad5,1";
+	if (strstr(u.machine, model)) {
+		if (strstr(u.version, "root:xnu-3789.22.3~1/RELEASE_ARM64_T7000")) {
+			printf("device %s matched\n", model);
 			init_iPad5_1_T7000();
 			return;
 		}
 		if (strstr(u.version, "root:xnu-3789.22.3~1/RELEASE_ARM64_T7001")) {
-			printf("device iPad5,1 matched\n");
+			printf("device %s matched\n", model);
 			init_iPad5_1_T7001();
 			return;
 		}
 		if (strstr(u.version, "root:xnu-3789.22.3~1/RELEASE_ARM64_S5L8960X")) {
-			printf("device iPad5,1 matched\n");
+			printf("device %s matched\n", model);
 			init_iPad5_1_S5L8960X();
 			return;
 		}
 	}
 	
-	if (strstr(u.machine, "iPad5,4")) {
+#pragma mark iPad5,2
+	model = "iPad5,2";
+	if (strstr(u.machine, model)) {
 		if (strstr(u.version, "root:xnu-3789.22.3~1/RELEASE_ARM64_T7000")) {
-			printf("device iPad5,4 matched\n");
+			printf("device %s matched\n", model);
+			init_iPad5_2_T7000();
+			return;
+		}
+		if (strstr(u.version, "root:xnu-3789.22.3~1/RELEASE_ARM64_T7001")) {
+			printf("device %s matched\n", model);
+			init_iPad5_2_T7001();
+			return;
+		}
+		if (strstr(u.version, "root:xnu-3789.22.3~1/RELEASE_ARM64_S5L8960X")) {
+			printf("device %s matched\n", model);
+			init_iPad5_2_S5L8960X();
+			return;
+		}
+	}
+	
+#pragma mark iPad5,3
+	model = "iPad5,3";
+	if (strstr(u.machine, model)) {
+		if (strstr(u.version, "root:xnu-3789.22.3~1/RELEASE_ARM64_T7000")) {
+			printf("device %s matched\n", model);
+			init_iPad5_3_T7000();
+			return;
+		}
+		if (strstr(u.version, "root:xnu-3789.22.3~1/RELEASE_ARM64_T7001")) {
+			printf("device %s matched\n", model);
+			init_iPad5_3_T7001();
+			return;
+		}
+		if (strstr(u.version, "root:xnu-3789.22.3~1/RELEASE_ARM64_S5L8960X")) {
+			printf("device %s matched\n", model);
+			init_iPad5_3_S5L8960X();
+			return;
+		}
+	}
+	
+#pragma mark iPad5,4
+	model = "iPad5,4";
+	if (strstr(u.machine, model)) {
+		if (strstr(u.version, "root:xnu-3789.22.3~1/RELEASE_ARM64_T7000")) {
+			printf("device %s matched\n", model);
 			init_iPad5_4_T7000();
 			return;
 		}
 		if (strstr(u.version, "root:xnu-3789.22.3~1/RELEASE_ARM64_T7001")) {
-			printf("device iPad5,4 matched\n");
+			printf("device %s matched\n", model);
 			init_iPad5_4_T7001();
 			return;
 		}
 		if (strstr(u.version, "root:xnu-3789.22.3~1/RELEASE_ARM64_S5L8960X")) {
-			printf("device iPad5,4 matched\n");
+			printf("device %s matched\n", model);
 			init_iPad5_4_S5L8960X();
 			return;
 		}
 	}
 	
-	if (strstr(u.machine, "iPad6,3")) {
+#pragma mark iPad6,3
+	model = "iPad6,3";
+	if (strstr(u.machine, model)) {
 		if (strstr(u.version, "root:xnu-3789.22.3~1/RELEASE_ARM64_S8000")) {
-			printf("device iPad6,3 matched\n");
+			printf("device %s matched\n", model);
 			init_iPad6_3_S8000();
 			return;
 		}
 	}
 	
-	if (strstr(u.machine, "iPad6,4")) {
+#pragma mark iPad6,4
+	model = "iPad6,4";
+	if (strstr(u.machine, model)) {
 		if (strstr(u.version, "root:xnu-3789.22.3~1/RELEASE_ARM64_S8000")) {
-			printf("device iPad6,4 matched\n");
+			printf("device %s matched\n", model);
 			init_iPad6_4_S8000();
 			return;
 		}
 	}
 	
-	if (strstr(u.machine, "iPad6,8")) {
+#pragma mark iPad6,7
+	model = "iPad6,7";
+	if (strstr(u.machine, model)) {
 		if (strstr(u.version, "root:xnu-3789.22.3~1/RELEASE_ARM64_S8000")) {
-			printf("device iPad6,8 matched\n");
+			printf("device %s matched\n", model);
+			init_iPad6_7_S8000();
+			return;
+		}
+	}
+	
+#pragma mark iPad6,8
+	model = "iPad6,8";
+	if (strstr(u.machine, model)) {
+		if (strstr(u.version, "root:xnu-3789.22.3~1/RELEASE_ARM64_S8000")) {
+			printf("device %s matched\n", model);
 			init_iPad6_8_S8000();
 			return;
 		}
